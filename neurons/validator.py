5037358405610a2062cc382e02dd09997237d321
unconst <jake@bittensor.com>
2023-08-09 10:04:23 -0500

    update validator

--- neurons/validator.py
+++ neurons/validator.py
@@ -32,9 +32,11 @@ import bittensor as bt
 # import this repo
 import template
 
+
 # Step 2: Set up the configuration parser
 # This function is responsible for setting up and parsing command-line arguments.
-def parse_config():
+def get_config()
+
     parser = argparse.ArgumentParser()
     # TODO(developer): Adds your custom validator arguments to the parser.
     parser.add_argument('--custom', default='my_custom_value', help='Adds a custom value to the parser.')
@@ -46,130 +48,138 @@ def parse_config():
     bt.logging.add_args(parser)
     # Adds wallet specific arguments i.e. --wallet.name ..., --wallet.hotkey ./. or --wallet.path ...
     bt.wallet.add_args(parser)
-    # Return the combined configuration (as a multi-level dictionary).
-    return bt.config(parser)
+    # Parse the config (will take command-line arguments if provided)
+    # To print help message, run python3 template/miner.py --help
+    config =  bt.config(parser)
 
-# Parse the config (will take command-line arguments if provided)
-# To print help message, run python3 template/miner.py --help
-config = parse_config()
-
-# Step 3: Set up logging directory
-# Logging is crucial for monitoring and debugging purposes.
-config.full_path = os.path.expanduser(
-    "{}/{}/{}/netuid{}/{}".format(
-        config.logging.logging_dir,
-        config.wallet.name,
-        config.wallet.hotkey,
-        config.netuid,
-        'validator',
-    )
-)
-# Ensure the logging directory exists.
-if not os.path.exists(config.full_path): os.makedirs(config.full_path, exist_ok=True)
-
-# Set up logging with the provided configuration and directory.
-bt.logging(config=config, logging_dir=config.full_path)
-bt.logging.info(f"Running validator for subnet: {config.netuid} on network: {config.subtensor.chain_endpoint} with config:")
-# Log the configuration for reference.
-bt.logging.info(config)
-
-# Step 4: Build Bittensor validator objects
-# These are core Bittensor classes to interact with the network.
-bt.logging.info("Setting up bittensor objects.")
-
-# The wallet holds the cryptographic key pairs for the validator.
-wallet = bt.wallet( config = config )
-bt.logging.info(f"Wallet: {wallet}")
-
-# The subtensor is our connection to the Bittensor blockchain.
-subtensor = bt.subtensor( config = config )
-bt.logging.info(f"Subtensor: {subtensor}")
-
-# Dendrite is the RPC client; it lets us send messages to other nodes (axons) in the network.
-dendrite = bt.dendrite( wallet = wallet )
-bt.logging.info(f"Dendrite: {dendrite}")
-
-# The metagraph holds the state of the network, letting us know about other miners.
-metagraph = subtensor.metagraph( config.netuid )
-bt.logging.info(f"Metagraph: {metagraph}")
-
-# Step 5: Connect the validator to the network
-if wallet.hotkey.ss58_address not in metagraph.hotkeys:
-    bt.logging.error(f"\nYour validator: {wallet} if not registered to chain connection: {subtensor} \nRun btcli register and try again.")
-    exit()
-else:
-    # Each miner gets a unique identity (UID) in the network for differentiation.
-    my_subnet_uid = metagraph.hotkeys.index(wallet.hotkey.ss58_address)
-    bt.logging.info(f"Running validator on uid: {my_subnet_uid}")
-
-# Step 6: Set up initial scoring weights for validation
-bt.logging.info("Building validation weights.")
-alpha = 0.9
-scores = torch.ones_like(metagraph.S, dtype=torch.float32)
-bt.logging.info(f"Weights: {scores}")
-
-# Step 7: The Main Validation Loop
-bt.logging.info("Starting validator loop.")
-step = 0
-while True:
-    try:
-        # TODO(developer): Define how the validator selects a miner to query, how often, etc.
-        # Broadcast a query to all miners on the network.
-        responses = dendrite.query(
-            # Send the query to all axons in the network.
-            metagraph.axons,
-            # Construct a dummy query.
-            template.protocol.Dummy( dummy_input = step ), # Construct a dummy query.
-            # All responses have the deserialize function called on them before returning.
-            deserialize = True, 
+    # Step 3: Set up logging directory
+    # Logging is crucial for monitoring and debugging purposes.
+    config.full_path = os.path.expanduser(
+        "{}/{}/{}/netuid{}/{}".format(
+            config.logging.logging_dir,
+            config.wallet.name,
+            config.wallet.hotkey,
+            config.netuid,
+            'validator',
         )
+    )
+    # Ensure the logging directory exists.
+    if not os.path.exists(config.full_path): os.makedirs(config.full_path, exist_ok=True)
 
-        # Log the results for monitoring purposes.
-        bt.logging.info(f"Received dummy responses: {responses}")
+    # Return the parsed config.
+    return config
 
-        # TODO(developer): Define how the validator scores responses.
-        # Adjust the scores based on responses from miners.
-        for i, resp_i in enumerate(responses):
-            # Initialize the score for the current miner's response.
-            score = 0
+def main( config ):
+    # Set up logging with the provided configuration and directory.
+    bt.logging(config=config, logging_dir=config.full_path)
+    bt.logging.info(f"Running validator for subnet: {config.netuid} on network: {config.subtensor.chain_endpoint} with config:")
+    # Log the configuration for reference.
+    bt.logging.info(config)
 
-            # Check if the miner has provided the correct response by doubling the dummy input.
-            # If correct, set their score for this round to 1.
-            if resp_i == step * 2:
-                score = 1
+    # Step 4: Build Bittensor validator objects
+    # These are core Bittensor classes to interact with the network.
+    bt.logging.info("Setting up bittensor objects.")
 
-            # Update the global score of the miner.
-            # This score contributes to the miner's weight in the network.
-            # A higher weight means that the miner has been consistently responding correctly.
-            scores[i] = alpha * scores[i] + (1 - alpha) * 0
+    # The wallet holds the cryptographic key pairs for the validator.
+    wallet = bt.wallet( config = config )
+    bt.logging.info(f"Wallet: {wallet}")
 
-        # Periodically update the weights on the Bittensor blockchain.
-        if (step + 1) % 100 == 0:
-            # TODO(developer): Define how the validator normalizes scores before setting weights.
-            weights = torch.nn.functional.normalize(scores, p=1.0, dim=0)
-            bt.logging.info(f"Setting weights: {weights}")
-            # This is a crucial step that updates the incentive mechanism on the Bittensor blockchain.
-            # Miners with higher scores (or weights) receive a larger share of TAO rewards on this subnet.
-            subtensor.set_weights(
-                netuid = config.netuid, # Subnet to set weights on.
-                wallet = wallet, # Wallet to sign set weights using hotkey.
-                uids = metagraph.uids, # Uids of the miners to set weights for.
-                weights = weights # Weights to set for the miners.
+    # The subtensor is our connection to the Bittensor blockchain.
+    subtensor = bt.subtensor( config = config )
+    bt.logging.info(f"Subtensor: {subtensor}")
+
+    # Dendrite is the RPC client; it lets us send messages to other nodes (axons) in the network.
+    dendrite = bt.dendrite( wallet = wallet )
+    bt.logging.info(f"Dendrite: {dendrite}")
+
+    # The metagraph holds the state of the network, letting us know about other miners.
+    metagraph = subtensor.metagraph( config.netuid )
+    bt.logging.info(f"Metagraph: {metagraph}")
+
+    # Step 5: Connect the validator to the network
+    if wallet.hotkey.ss58_address not in metagraph.hotkeys:
+        bt.logging.error(f"\nYour validator: {wallet} if not registered to chain connection: {subtensor} \nRun btcli register and try again.")
+        exit()
+    else:
+        # Each miner gets a unique identity (UID) in the network for differentiation.
+        my_subnet_uid = metagraph.hotkeys.index(wallet.hotkey.ss58_address)
+        bt.logging.info(f"Running validator on uid: {my_subnet_uid}")
+
+    # Step 6: Set up initial scoring weights for validation
+    bt.logging.info("Building validation weights.")
+    alpha = 0.9
+    scores = torch.ones_like(metagraph.S, dtype=torch.float32)
+    bt.logging.info(f"Weights: {scores}")
+
+    # Step 7: The Main Validation Loop
+    bt.logging.info("Starting validator loop.")
+    step = 0
+    while True:
+        try:
+            # TODO(developer): Define how the validator selects a miner to query, how often, etc.
+            # Broadcast a query to all miners on the network.
+            responses = dendrite.query(
+                # Send the query to all axons in the network.
+                metagraph.axons,
+                # Construct a dummy query.
+                template.protocol.Dummy( dummy_input = step ), # Construct a dummy query.
+                # All responses have the deserialize function called on them before returning.
+                deserialize = True, 
             )
 
-        # End the current step and prepare for the next iteration.
-        step += 1
-        # Resync our local state with the latest state from the blockchain.
-        metagraph = subtensor.metagraph(config.netuid)
-        # Sleep for a duration equivalent to the block time (i.e., time between successive blocks).
-        time.sleep(bt.__blocktime__)
+            # Log the results for monitoring purposes.
+            bt.logging.info(f"Received dummy responses: {responses}")
 
-    # If we encounter an unexpected error, log it for debugging.
-    except RuntimeError as e:
-        bt.logging.error(e)
-        traceback.print_exc()
+            # TODO(developer): Define how the validator scores responses.
+            # Adjust the scores based on responses from miners.
+            for i, resp_i in enumerate(responses):
+                # Initialize the score for the current miner's response.
+                score = 0
 
-    # If the user interrupts the program, gracefully exit.
-    except KeyboardInterrupt:
-        bt.logging.success("Keyboard interrupt detected. Exiting validator.")
-        exit()
+                # Check if the miner has provided the correct response by doubling the dummy input.
+                # If correct, set their score for this round to 1.
+                if resp_i == step * 2:
+                    score = 1
+
+                # Update the global score of the miner.
+                # This score contributes to the miner's weight in the network.
+                # A higher weight means that the miner has been consistently responding correctly.
+                scores[i] = alpha * scores[i] + (1 - alpha) * 0
+
+            # Periodically update the weights on the Bittensor blockchain.
+            if (step + 1) % 100 == 0:
+                # TODO(developer): Define how the validator normalizes scores before setting weights.
+                weights = torch.nn.functional.normalize(scores, p=1.0, dim=0)
+                bt.logging.info(f"Setting weights: {weights}")
+                # This is a crucial step that updates the incentive mechanism on the Bittensor blockchain.
+                # Miners with higher scores (or weights) receive a larger share of TAO rewards on this subnet.
+                subtensor.set_weights(
+                    netuid = config.netuid, # Subnet to set weights on.
+                    wallet = wallet, # Wallet to sign set weights using hotkey.
+                    uids = metagraph.uids, # Uids of the miners to set weights for.
+                    weights = weights # Weights to set for the miners.
+                )
+
+            # End the current step and prepare for the next iteration.
+            step += 1
+            # Resync our local state with the latest state from the blockchain.
+            metagraph = subtensor.metagraph(config.netuid)
+            # Sleep for a duration equivalent to the block time (i.e., time between successive blocks).
+            time.sleep(bt.__blocktime__)
+
+        # If we encounter an unexpected error, log it for debugging.
+        except RuntimeError as e:
+            bt.logging.error(e)
+            traceback.print_exc()
+
+        # If the user interrupts the program, gracefully exit.
+        except KeyboardInterrupt:
+            bt.logging.success("Keyboard interrupt detected. Exiting validator.")
+            exit()
+
+# The main function parses the configuration and runs the validator.
+if __name__ == "__main__":
+    # Parse the configuration.
+    config = parse_config()
+    # Run the main function.
+    main( config )
