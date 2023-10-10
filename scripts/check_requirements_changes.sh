c34a3164643bef003c7699e8327a6fcaf7b853b1
ifrit98 <ifrit98@gmail.com>
2023-10-10 23:15:36 +0000

    update correct req file path for check

--- scripts/check_requirements_changes.sh
+++ scripts/check_requirements_changes.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Check if requirements files have changed in the last commit
-if git diff --name-only HEAD~1 | grep -E 'requirements/prod.txt|requirements/dev.txt'; then
+if git diff --name-only HEAD~1 | grep -E 'requirements.txt|requirements.txt'; then
     echo "Requirements files have changed. Running compatibility checks..."
     echo 'export REQUIREMENTS_CHANGED="true"' >> $BASH_ENV
 else
