dd453d36aa3d0292d17ef1b4d55068630605eb4a
ifrit98 <ifrit98@gmail.com>
2023-10-10 15:30:32 +0000

    black

--- neurons/miner.py
+++ neurons/miner.py
@@ -31,15 +31,18 @@ import bittensor as bt
 # import this repo
 import template
 
+
 def get_config():
     # Step 2: Set up the configuration parser
     # This function initializes the necessary command-line arguments.
     # Using command-line arguments allows users to customize various miner settings.
     parser = argparse.ArgumentParser()
     # TODO(developer): Adds your custom miner arguments to the parser.
-    parser.add_argument('--custom', default='my_custom_value', help='Adds a custom value to the parser.')
+    parser.add_argument(
+        "--custom", default="my_custom_value", help="Adds a custom value to the parser."
+    )
     # Adds override arguments for network and netuid.
-    parser.add_argument( '--netuid', type = int, default = 1, help = "The chain subnet uid." )
+    parser.add_argument("--netuid", type=int, default=1, help="The chain subnet uid.")
     # Adds subtensor specific arguments i.e. --subtensor.chain_endpoint ... --subtensor.network ...
     bt.subtensor.add_args(parser)
     # Adds logging specific arguments i.e. --logging.debug ..., --logging.trace .. or --logging.logging_dir ...
@@ -60,20 +63,22 @@ def get_config():
             config.wallet.name,
             config.wallet.hotkey,
             config.netuid,
-            'miner',
+            "miner",
         )
     )
     # Ensure the directory for logging exists, else create one.
-    if not os.path.exists(config.full_path): os.makedirs(config.full_path, exist_ok=True)
+    if not os.path.exists(config.full_path):
+        os.makedirs(config.full_path, exist_ok=True)
     return config
 
 
 # Main takes the config and starts the miner.
-def main( config ):
-
+def main(config):
     # Activating Bittensor's logging with the set configurations.
     bt.logging(config=config, logging_dir=config.full_path)
-    bt.logging.info(f"Running miner for subnet: {config.netuid} on network: {config.subtensor.chain_endpoint} with config:")
+    bt.logging.info(
+        f"Running miner for subnet: {config.netuid} on network: {config.subtensor.chain_endpoint} with config:"
+    )
 
     # This logs the active configuration to the specified logging directory for review.
     bt.logging.info(config)
@@ -83,11 +88,11 @@ def main( config ):
     bt.logging.info("Setting up bittensor objects.")
 
     # Wallet holds cryptographic information, ensuring secure transactions and communication.
-    wallet = bt.wallet( config = config )
+    wallet = bt.wallet(config=config)
     bt.logging.info(f"Wallet: {wallet}")
 
     # subtensor manages the blockchain connection, facilitating interaction with the Bittensor blockchain.
-    subtensor = bt.subtensor( config = config )
+    subtensor = bt.subtensor(config=config)
     bt.logging.info(f"Subtensor: {subtensor}")
 
     # metagraph provides the network's current state, holding state about other participants in a subnet.
@@ -95,7 +100,9 @@ def main( config ):
     bt.logging.info(f"Metagraph: {metagraph}")
 
     if wallet.hotkey.ss58_address not in metagraph.hotkeys:
-        bt.logging.error(f"\nYour miner: {wallet} is not registered to chain connection: {subtensor} \nRun btcli register and try again. ")
+        bt.logging.error(
+            f"\nYour miner: {wallet} is not registered to chain connection: {subtensor} \nRun btcli register and try again. "
+        )
         exit()
 
     # Each miner gets a unique identity (UID) in the network for differentiation.
@@ -105,40 +112,48 @@ def main( config ):
     # Step 5: Set up miner functionalities
     # The following functions control the miner's response to incoming requests.
     # The blacklist function decides if a request should be ignored.
-    def blacklist_fn( synapse: template.protocol.Dummy ) -> typing.Tuple[bool, str]:
-        # TODO(developer): Define how miners should blacklist requests. This Function 
+    def blacklist_fn(synapse: template.protocol.Dummy) -> typing.Tuple[bool, str]:
+        # TODO(developer): Define how miners should blacklist requests. This Function
         # Runs before the synapse data has been deserialized (i.e. before synapse.data is available).
         # The synapse is instead contructed via the headers of the request. It is important to blacklist
         # requests before they are deserialized to avoid wasting resources on requests that will be ignored.
         # Below: Check that the hotkey is a registered entity in the metagraph.
         if synapse.dendrite.hotkey not in metagraph.hotkeys:
             # Ignore requests from unrecognized entities.
-            bt.logging.trace(f'Blacklisting unrecognized hotkey {synapse.dendrite.hotkey}')
+            bt.logging.trace(
+                f"Blacklisting unrecognized hotkey {synapse.dendrite.hotkey}"
+            )
             return True, "Unrecognized hotkey"
-        # TODO(developer): In practice it would be wise to blacklist requests from entities that 
+        # TODO(developer): In practice it would be wise to blacklist requests from entities that
         # are not validators, or do not have enough stake. This can be checked via metagraph.S
         # and metagraph.validator_permit. You can always attain the uid of the sender via a
         # metagraph.hotkeys.index( synapse.dendrite.hotkey ) call.
         # Otherwise, allow the request to be processed further.
-        bt.logging.trace(f'Not Blacklisting recognized hotkey {synapse.dendrite.hotkey}')
+        bt.logging.trace(
+            f"Not Blacklisting recognized hotkey {synapse.dendrite.hotkey}"
+        )
         return False, "Hotkey recognized!"
 
     # The priority function determines the order in which requests are handled.
     # More valuable or higher-priority requests are processed before others.
-    def priority_fn( synapse: template.protocol.Dummy ) -> float:
+    def priority_fn(synapse: template.protocol.Dummy) -> float:
         # TODO(developer): Define how miners should prioritize requests.
         # Miners may recieve messages from multiple entities at once. This function
         # determines which request should be processed first. Higher values indicate
         # that the request should be processed first. Lower values indicate that the
         # request should be processed later.
         # Below: simple logic, prioritize requests from entities with more stake.
-        caller_uid = metagraph.hotkeys.index( synapse.dendrite.hotkey ) # Get the caller index.
-        prirority = float( metagraph.S[ caller_uid ] ) # Return the stake as the priority.
-        bt.logging.trace(f'Prioritizing {synapse.dendrite.hotkey} with value: ', prirority)
+        caller_uid = metagraph.hotkeys.index(
+            synapse.dendrite.hotkey
+        )  # Get the caller index.
+        prirority = float(metagraph.S[caller_uid])  # Return the stake as the priority.
+        bt.logging.trace(
+            f"Prioritizing {synapse.dendrite.hotkey} with value: ", prirority
+        )
         return prirority
 
     # This is the core miner function, which decides the miner's response to a valid, high-priority request.
-    def dummy( synapse: template.protocol.Dummy ) -> template.protocol.Dummy:
+    def dummy(synapse: template.protocol.Dummy) -> template.protocol.Dummy:
         # TODO(developer): Define how miners should process requests.
         # This function runs after the synapse has been deserialized (i.e. after synapse.data is available).
         # This function runs after the blacklist and priority functions have been called.
@@ -149,21 +164,23 @@ def main( config ):
 
     # Step 6: Build and link miner functions to the axon.
     # The axon handles request processing, allowing validators to send this process requests.
-    axon = bt.axon( wallet = wallet, config=config )
+    axon = bt.axon(wallet=wallet, config=config)
     bt.logging.info(f"Axon {axon}")
 
     # Attach determiners which functions are called when servicing a request.
     bt.logging.info(f"Attaching forward function to axon.")
     axon.attach(
-        forward_fn = dummy,
-        blacklist_fn = blacklist_fn,
-        priority_fn = priority_fn,
+        forward_fn=dummy,
+        blacklist_fn=blacklist_fn,
+        priority_fn=priority_fn,
     )
 
     # Serve passes the axon information to the network + netuid we are hosting on.
     # This will auto-update if the axon port of external ip have changed.
-    bt.logging.info(f"Serving axon {dummy} on network: {config.subtensor.chain_endpoint} with netuid: {config.netuid}")
-    axon.serve( netuid = config.netuid, subtensor = subtensor )
+    bt.logging.info(
+        f"Serving axon {dummy} on network: {config.subtensor.chain_endpoint} with netuid: {config.netuid}"
+    )
+    axon.serve(netuid=config.netuid, subtensor=subtensor)
 
     # Start  starts the miner's axon, making it active on the network.
     bt.logging.info(f"Starting axon server on port: {config.axon.port}")
@@ -179,14 +196,16 @@ def main( config ):
             # Below: Periodically update our knowledge of the network graph.
             if step % 5 == 0:
                 metagraph = subtensor.metagraph(config.netuid)
-                log =  (f'Step:{step} | '\
-                        f'Block:{metagraph.block.item()} | '\
-                        f'Stake:{metagraph.S[my_subnet_uid]} | '\
-                        f'Rank:{metagraph.R[my_subnet_uid]} | '\
-                        f'Trust:{metagraph.T[my_subnet_uid]} | '\
-                        f'Consensus:{metagraph.C[my_subnet_uid] } | '\
-                        f'Incentive:{metagraph.I[my_subnet_uid]} | '\
-                        f'Emission:{metagraph.E[my_subnet_uid]}')
+                log = (
+                    f"Step:{step} | "
+                    f"Block:{metagraph.block.item()} | "
+                    f"Stake:{metagraph.S[my_subnet_uid]} | "
+                    f"Rank:{metagraph.R[my_subnet_uid]} | "
+                    f"Trust:{metagraph.T[my_subnet_uid]} | "
+                    f"Consensus:{metagraph.C[my_subnet_uid] } | "
+                    f"Incentive:{metagraph.I[my_subnet_uid]} | "
+                    f"Emission:{metagraph.E[my_subnet_uid]}"
+                )
                 bt.logging.info(log)
             step += 1
             time.sleep(1)
@@ -194,7 +213,7 @@ def main( config ):
         # If someone intentionally stops the miner, it'll safely terminate operations.
         except KeyboardInterrupt:
             axon.stop()
-            bt.logging.success('Miner killed by keyboard interrupt.')
+            bt.logging.success("Miner killed by keyboard interrupt.")
             break
         # In case of unforeseen errors, the miner will log the error and continue operations.
         except Exception as e:
@@ -204,4 +223,4 @@ def main( config ):
 
 # This is the main function, which runs the miner.
 if __name__ == "__main__":
-    main( get_config() )
+    main(get_config())
--- setup.py
+++ setup.py
@@ -26,19 +26,20 @@ from io import open
 from setuptools import setup, find_packages
 from pkg_resources import parse_requirements
 
+
 def read_requirements(path):
-    with open(path, 'r') as f:
+    with open(path, "r") as f:
         requirements = f.read().splitlines()
         processed_requirements = []
-        
+
         for req in requirements:
             # For git or other VCS links
-            if req.startswith('git+') or '@' in req:
-                pkg_name = re.search(r'(#egg=)([\w\-_]+)', req)
+            if req.startswith("git+") or "@" in req:
+                pkg_name = re.search(r"(#egg=)([\w\-_]+)", req)
                 if pkg_name:
                     processed_requirements.append(pkg_name.group(2))
                 else:
-                    # You may decide to raise an exception here, 
+                    # You may decide to raise an exception here,
                     # if you want to ensure every VCS link has an #egg=<package_name> at the end
                     continue
             else:
@@ -46,47 +47,50 @@ def read_requirements(path):
         return processed_requirements
 
 
-requirements = read_requirements('requirements.txt')
+requirements = read_requirements("requirements.txt")
 here = path.abspath(path.dirname(__file__))
 
-with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+with open(path.join(here, "README.md"), encoding="utf-8") as f:
     long_description = f.read()
 
 # loading version from setup.py
-with codecs.open(os.path.join(here, 'template/__init__.py'), encoding='utf-8') as init_file:
-    version_match = re.search(r"^__version__ = ['\"]([^'\"]*)['\"]", init_file.read(), re.M)
+with codecs.open(
+    os.path.join(here, "template/__init__.py"), encoding="utf-8"
+) as init_file:
+    version_match = re.search(
+        r"^__version__ = ['\"]([^'\"]*)['\"]", init_file.read(), re.M
+    )
     version_string = version_match.group(1)
 
 setup(
-    name='bittensor_subnet_template', # TODO(developer): Change this value to your module subnet name.
-    version=version_string,    
-    description='bittensor_subnet_template', # TODO(developer): Change this value to your module subnet description.
+    name="bittensor_subnet_template",  # TODO(developer): Change this value to your module subnet name.
+    version=version_string,
+    description="bittensor_subnet_template",  # TODO(developer): Change this value to your module subnet description.
     long_description=long_description,
-    long_description_content_type='text/markdown',
-    url='https://github.com/opentensor/bittensor-subnet-template', # TODO(developer): Change this url to your module subnet github url.
-    author='bittensor.com', # TODO(developer): Change this value to your module subnet author name.
+    long_description_content_type="text/markdown",
+    url="https://github.com/opentensor/bittensor-subnet-template",  # TODO(developer): Change this url to your module subnet github url.
+    author="bittensor.com",  # TODO(developer): Change this value to your module subnet author name.
     packages=find_packages(),
     include_package_data=True,
-    author_email='', # TODO(developer): Change this value to your module subnet author email.
-    license='MIT',
-    python_requires='>=3.8',
+    author_email="",  # TODO(developer): Change this value to your module subnet author email.
+    license="MIT",
+    python_requires=">=3.8",
     install_requires=requirements,
     classifiers=[
-        'Development Status :: 3 - Alpha',
-        'Intended Audience :: Developers',
-        'Topic :: Software Development :: Build Tools',
-
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "Topic :: Software Development :: Build Tools",
         # Pick your license as you wish
-        'License :: OSI Approved :: MIT License',
-        'Programming Language :: Python :: 3 :: Only',
-        'Programming Language :: Python :: 3.8',
-        'Programming Language :: Python :: 3.9',
-        'Programming Language :: Python :: 3.10',
-        'Topic :: Scientific/Engineering',
-        'Topic :: Scientific/Engineering :: Mathematics',
-        'Topic :: Scientific/Engineering :: Artificial Intelligence',
-        'Topic :: Software Development',
-        'Topic :: Software Development :: Libraries',
-        'Topic :: Software Development :: Libraries :: Python Modules',
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3 :: Only",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Topic :: Scientific/Engineering",
+        "Topic :: Scientific/Engineering :: Mathematics",
+        "Topic :: Scientific/Engineering :: Artificial Intelligence",
+        "Topic :: Software Development",
+        "Topic :: Software Development :: Libraries",
+        "Topic :: Software Development :: Libraries :: Python Modules",
     ],
 )
