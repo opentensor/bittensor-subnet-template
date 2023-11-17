7e386b9fcfaf73596724600e1c2cc858de644114
aph5nt <dorotawojciechowska85@gmail.com>
2023-11-17 09:24:45 +0100

    ver bump

--- insights/__init__.py
+++ insights/__init__.py
@@ -17,7 +17,7 @@ OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE S
 DEALINGS IN THE SOFTWARE.
 """
 
-__version__ = "0.0.1"
+__version__ = "0.1.0"
 version_split = __version__.split(".")
 __spec_version__ = (
     (1000 * int(version_split[0]))
