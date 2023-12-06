6e6d0f94c1544aa4a4218d8fc4a72da3c604de8c
ifrit98 <ifrit98@gmail.com>
2023-12-06 21:52:13 +0000

    fix install script

--- scripts/install_staging.sh
+++ scripts/install_staging.sh
@@ -12,8 +12,11 @@ source "$HOME/.cargo/env"
 
 # Clone subtensor and enter the directory
 cd ../
-git clone https://github.com/opentensor/subtensor.git
+if [ ! -d "subtensor" ]; then
+    git clone https://github.com/opentensor/subtensor.git
+fi
 cd subtensor
+git pull
 
 # Update to the nightly version of rust
 ./scripts/init.sh
@@ -38,9 +41,10 @@ btcli wallet new_hotkey --wallet.name validator --wallet.hotkey default
 
 ## Setup localnet
 # Initialize your local subtensor chain in development mode. This command will set up and run a local subtensor network.
+cd ../subtensor
 
 # Start a new tmux session and create a new pane, but do not switch to it
-echo "export BT_DEFAULT_TOKEN_WALLET=\$(cat ~/.bittensor/wallets/owner2/coldkeypub.txt | grep -oP '\"ss58Address\": \"\K[^\"]+')" > setup_and_run.sh
+echo "export BT_DEFAULT_TOKEN_WALLET=\$(cat ~/.bittensor/wallets/owner/coldkeypub.txt | grep -oP '\"ss58Address\": \"\K[^\"]+')" > setup_and_run.sh
 echo "bash ./scripts/localnet.sh" >> setup_and_run.sh
 chmod +x setup_and_run.sh
 tmux new-session -d -s localnet -n 'localnet'
@@ -51,12 +55,12 @@ echo "localnet.sh is running in a detached tmux session named 'localnet'"
 echo "You can attach to this session with: tmux attach-session -t localnet"
 
 # Transfer tokens to miner and validator coldkeys
-export BT_OWNER_TOKEN_WALLET=$(cat ~/.bittensor/wallets/owner/coldkeypub.txt | grep -oP '"ss58Address": "\K[^"]+')
-export BT_MINER_TOKEN_WALLET=$(cat ~/.bittensor/wallets/miner/coldkeypub.txt | grep -oP '"ss58Address": "\K[^"]+')
-export BT_VALIDATOR_TOKEN_WALLET=$(cat ~/.bittensor/wallets/validator/coldkeypub.txt | grep -oP '"ss58Address": "\K[^"]+')
+export BT_owner_TOKEN_WALLET=$(cat ~/.bittensor/wallets/owner/coldkeypub.txt | grep -oP '"ss58Address": "\K[^"]+')
+export BT_miner_TOKEN_WALLET=$(cat ~/.bittensor/wallets/miner/coldkeypub.txt | grep -oP '"ss58Address": "\K[^"]+')
+export BT_validator_TOKEN_WALLET=$(cat ~/.bittensor/wallets/validator/coldkeypub.txt | grep -oP '"ss58Address": "\K[^"]+')
 
-btcli wallet transfer --subtensor.network ws://127.0.0.1:9946 --wallet.name owner --dest $BT_MINER_TOKEN_WALLET --amount 1000 --no_prompt
-btcli wallet transfer --subtensor.network ws://127.0.0.1:9946 --wallet.name owner --dest $BT_VALIDATOR_TOKEN_WALLET --amount 1000 --no_prompt
+btcli wallet transfer --subtensor.network ws://127.0.0.1:9946 --wallet.name owner --dest $BT_miner_TOKEN_WALLET --amount 1000 --no_prompt
+btcli wallet transfer --subtensor.network ws://127.0.0.1:9946 --wallet.name owner --dest $BT_validator_TOKEN_WALLET --amount 1000 --no_prompt
 
 # Register a subnet
 btcli subnet create --wallet.name owner --wallet.hotkey default --subtensor.chain_endpoint ws://127.0.0.1:9946 --no_prompt
@@ -73,9 +77,8 @@ btcli subnet list --subtensor.chain_endpoint ws://127.0.0.1:9946
 btcli wallet overview --wallet.name validator --subtensor.chain_endpoint ws://127.0.0.1:9946 --no_prompt
 btcli wallet overview --wallet.name miner --subtensor.chain_endpoint ws://127.0.0.1:9946 --no_prompt
 
-# TODO: Send to tmux and open a 2-pane window
-python neurons/miner.py --netuid 1 --subtensor.chain_endpoint ws://127.0.0.1:9946 --wallet.name miner --wallet.hotkey default --logging.debug
-python neurons/validator.py --netuid 1 --subtensor.chain_endpoint ws://127.0.0.1:9946 --wallet.name validator --wallet.hotkey default --logging.debug
+cd ../bittensor-subnet-template
+
 
 # Check if inside a tmux session
 if [ -z "$TMUX" ]; then
@@ -90,5 +93,5 @@ if [ -z "$TMUX" ]; then
 else
     # If already in a tmux session, create two panes in the current window
     tmux split-window -h 'python neurons/miner.py --netuid 1 --subtensor.chain_endpoint ws://127.0.0.1:9946 --wallet.name miner --wallet.hotkey default --logging.debug'
-    tmux split-window -v -t 0 'python neurons/validator.py --netuid 1 --subtensor.chain_endpoint ws://127.0.0.1:9946 --wallet.name validator --wallet.hotkey default --logging.debug'
+    tmux split-window -v -t 0 'python neurons/validator.py --netuid 1 --subtensor.chain_endpoint ws://127.0.0.1:9946 --wallet.name3 validator --wallet.hotkey default --logging.debug'
 fi
