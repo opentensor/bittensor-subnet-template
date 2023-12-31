0f7494117f6dd61600607f3b841bfd47c925696c
aph5nt <dorotawojciechowska85@gmail.com>
2023-12-31 19:55:33 +0100

    added support for custom subtensor

--- scripts/run_validator_mainnet.sh
+++ scripts/run_validator_mainnet.sh
@@ -20,12 +20,8 @@ if [ -z "$BITCOIN_NODE_RPC_URL" ]; then
     export BITCOIN_NODE_RPC_URL="http://bitcoinrpc:rpcpassword@127.0.0.1:8332"
 fi
 
-if [ -z "$SUBTENSOR_URL" ]; then
-    export SUBTENSOR_URL=wss://51.158.60.18:9946
-fi
-
 if [ -z "$SUBTENSOR_NETWORK" ]; then
-    export SUBTENSOR_NETWORK=local
+    export SUBTENSOR_NETWORK=51.15.237.39:9946
 fi
 
-python3 neurons/validators/validator.py --bitcoin_cheat_factor_sample_size "$BITCOIN_CHEAT_FACTOR_SAMPLE_SIZE" --wallet.name "$WALLET_NAME" --wallet.hotkey "$WALLET_HOTKEY" --netuid 15 --subtensor.network "$SUBTENSOR_NETWORK" --subtensor.chain_endpoint "$SUBTENSOR_URL" --logging.debug --logging.trace
+python3 neurons/validators/validator.py --bitcoin_cheat_factor_sample_size "$BITCOIN_CHEAT_FACTOR_SAMPLE_SIZE" --wallet.name "$WALLET_NAME" --wallet.hotkey "$WALLET_HOTKEY" --netuid 15 --subtensor.network "$SUBTENSOR_NETWORK" --logging.trace
