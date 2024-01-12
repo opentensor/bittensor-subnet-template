0af0e0089b3ebe9ec896e1a5c969e8fa7ddfa505
ifrit98 <ifrit98@gmail.com>
2024-01-12 15:33:33 +0000

    black format

--- template/utils/config.py
+++ template/utils/config.py
@@ -63,9 +63,7 @@ def add_args(cls, parser):
     # Netuid Arg: The netuid of the subnet to connect to.
     parser.add_argument("--netuid", type=int, help="Subnet netuid", default=1)
 
-    neuron_type = (
-        "validator" if "miner" not in cls.__name__.lower() else "miner"
-    )
+    neuron_type = "validator" if "miner" not in cls.__name__.lower() else "miner"
 
     parser.add_argument(
         "--neuron.name",
