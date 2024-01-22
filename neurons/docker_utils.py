159bf773e6498d76dd37215576c5131d85190703
Nicolas Boskovic <nboskovic@gmail.com>
2024-01-22 13:55:40 -0300

    Read docker image version from env

--- neurons/docker_utils.py
+++ neurons/docker_utils.py
@@ -1,25 +1,7 @@
-import socket
-import docker
-import bittensor as bt
+
+import os
+
 
 def get_docker_image_version():
-    return 'n/a'
-    """ temporary disable docker image version
-    try:
-        container_id = socket.gethostname()
-        client = docker.from_env()
-        container = client.containers.get(container_id)
-        image_details = container.image.tags
-        image_details = [x for x in image_details if x != 'latest']
-        if len(image_details) > 0:
-            return image_details[0]
-        else:
-            bt.logging.error(f"Could not find docker container with id: {container_id}")
-            return 'not found'
-    except docker.errors.NotFound as e:
-        bt.logging.error(f"Could not find docker container with id: {container_id}")
-        return 'n/a'
-    except Exception as e:
-        bt.logging.error(f"Could not get docker image version: {e}")
-        return 'n/a'
-    """
+    return os.getenv('VERSION', 'n/a')
+
