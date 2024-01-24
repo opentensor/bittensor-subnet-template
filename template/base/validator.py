5f8795c33733401f422613fe77c945ebe677f232
Maxim Bladyko <bladyko@gmail.com>
2024-01-24 21:57:08 +0300

    #1

--- template/base/validator.py
+++ template/base/validator.py
@@ -216,9 +216,7 @@ class BaseValidatorNeuron(BaseNeuron):
 
         # Calculate the average reward for each uid across non-zero values.
         # Replace any NaN values with 0.
-        raw_weights = torch.nn.functional.normalize(
-            self.moving_averaged_scores, p=1, dim=0
-        )
+        raw_weights = torch.nn.functional.normalize(self.scores, p=1, dim=0)
 
         bt.logging.debug("raw_weights", raw_weights)
         bt.logging.debug("raw_weight_uids", self.metagraph.uids.to("cpu"))
