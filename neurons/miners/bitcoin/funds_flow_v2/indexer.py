ce9cc94fbd99e07fb6179e937f1e88f447da206f
ronnie-samaroo <ron.sam624@gmail.com>
2024-01-29 21:46:36 +0100

    fix: infinite loop on indexer

--- neurons/miners/bitcoin/funds_flow_v2/indexer.py
+++ neurons/miners/bitcoin/funds_flow_v2/indexer.py
@@ -24,77 +24,74 @@ def shutdown_handler(signum, frame):
 
 def index_blocks(_bitcoin_node, _graph_creator, _graph_indexer, start_height, end_height):
     global shutdown_flag
-    skip_blocks = 6
 
-    while not shutdown_flag:
-        # current_block_height = _bitcoin_node.get_current_block_height() - 6
-        # if current_block_height - skip_blocks < 0:
-        #     logger.info("Waiting min 6 for blocks to be mined.")
-        #     time.sleep(10)
-        #     continue
+    # current_block_height = _bitcoin_node.get_current_block_height() - 6
+    # if current_block_height - skip_blocks < 0:
+    #     logger.info("Waiting min 6 for blocks to be mined.")
+    #     time.sleep(10)
+    #     continue
 
-        # if start_height > current_block_height:
-        #     logger.info(
-        #         f"Waiting for new blocks. Current height is {current_block_height}."
-        #     )
-        #     time.sleep(10)
-        #     continue
+    # if start_height > current_block_height:
+    #     logger.info(
+    #         f"Waiting for new blocks. Current height is {current_block_height}."
+    #     )
+    #     time.sleep(10)
+    #     continue
 
-        block_height = start_height
-        while block_height >= end_height:
-            block = _bitcoin_node.get_block_by_height(block_height)
-            num_transactions = len(block["tx"])
-            start_time = time.time()
-            in_memory_graph = _graph_creator.create_in_memory_graph_from_block(block)
-            success = _graph_indexer.create_graph_focused_on_money_flow(in_memory_graph, _bitcoin_node)
-            end_time = time.time()
-            time_taken = end_time - start_time
-            node_block_height = bitcoin_node.get_current_block_height()
-            progress = block_height / node_block_height * 100
-            formatted_num_transactions = "{:>4}".format(num_transactions)
-            formatted_time_taken = "{:6.2f}".format(time_taken)
-            formatted_tps = "{:8.2f}".format(
-                num_transactions / time_taken if time_taken > 0 else float("inf")
+    block_height = start_height
+    while block_height >= end_height and not shutdown_flag:
+        block = _bitcoin_node.get_block_by_height(block_height)
+        num_transactions = len(block["tx"])
+        start_time = time.time()
+        in_memory_graph = _graph_creator.create_in_memory_graph_from_block(block)
+        success = _graph_indexer.create_graph_focused_on_money_flow(in_memory_graph, _bitcoin_node)
+        end_time = time.time()
+        time_taken = end_time - start_time
+        node_block_height = bitcoin_node.get_current_block_height()
+        progress = block_height / node_block_height * 100
+        formatted_num_transactions = "{:>4}".format(num_transactions)
+        formatted_time_taken = "{:6.2f}".format(time_taken)
+        formatted_tps = "{:8.2f}".format(
+            num_transactions / time_taken if time_taken > 0 else float("inf")
+        )
+        formatted_progress = "{:6.2f}".format(progress)
+
+        if time_taken > 0:
+            logger.info(
+                "Block {:>6}: Processed {} transactions in {} seconds {} TPS Progress: {}%".format(
+                    block_height,
+                    formatted_num_transactions,
+                    formatted_time_taken,
+                    formatted_tps,
+                    formatted_progress,
+                )
             )
-            formatted_progress = "{:6.2f}".format(progress)
-
-            if time_taken > 0:
-                logger.info(
-                    "Block {:>6}: Processed {} transactions in {} seconds {} TPS Progress: {}%".format(
-                        block_height,
-                        formatted_num_transactions,
-                        formatted_time_taken,
-                        formatted_tps,
-                        formatted_progress,
-                    )
+        else:
+            logger.info(
+                "Block {:>6}: Processed {} transactions in 0.00 seconds (  Inf TPS). Progress: {}%".format(
+                    block_height, formatted_num_transactions, formatted_progress
                 )
-            else:
-                logger.info(
-                    "Block {:>6}: Processed {} transactions in 0.00 seconds (  Inf TPS). Progress: {}%".format(
-                        block_height, formatted_num_transactions, formatted_progress
-                    )
-                )
+            )
 
-            if success:
-                block_height -= 1
+        if success:
+            block_height -= 1
 
-                # # indexer flooding prevention
-                # threshold = int(os.getenv('BLOCK_PROCESSING_TRANSACTION_THRESHOLD', 500))
-                # if num_transactions > threshold:
-                #     delay = float(os.getenv('BLOCK_PROCESSING_DELAY', 1))
-                #     logger.info(f"Block tx count above {threshold}, slowing down indexing by {delay} seconds to prevent flooding.")
-                #     time.sleep(delay)
+            # # indexer flooding prevention
+            # threshold = int(os.getenv('BLOCK_PROCESSING_TRANSACTION_THRESHOLD', 500))
+            # if num_transactions > threshold:
+            #     delay = float(os.getenv('BLOCK_PROCESSING_DELAY', 1))
+            #     logger.info(f"Block tx count above {threshold}, slowing down indexing by {delay} seconds to prevent flooding.")
+            #     time.sleep(delay)
 
-            else:
-                logger.error(f"Failed to index block {block_height}.")
-                time.sleep(30)
+        else:
+            logger.error(f"Failed to index block {block_height}.")
+            time.sleep(30)
 
-            if shutdown_flag:
-                logger.info(f"Finished indexing block {block_height} before shutdown.")
-                break
+        if shutdown_flag:
+            logger.info(f"Finished indexing block {block_height} before shutdown.")
+            break
 
-            # start_height += 1
-
+        # start_height += 1
 
 
 # Register the shutdown handler for SIGINT and SIGTERM
@@ -120,7 +117,7 @@ if __name__ == "__main__":
 
         retry_delay = 60
         # purpose of this indexer is to index FROM to infinity only, indexing previous block range will be covered by another indexer - indexer_patch.py which will be slowly adding previous blocks to the graph
-        while True:
+        while not shutdown_flag:
             try:
                 logger.info("Starting indexer")
                 graph_last_block_height = graph_indexer.get_latest_block_number()
