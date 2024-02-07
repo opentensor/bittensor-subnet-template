dee383d179f4bf46e3dd3cbd24fea4e0a87dec70
Alexandre Desroches <alex.desroches7@gmail.com>
2024-02-06 21:08:17 -0500

    fix base miner.py axon init params

--- template/base/miner.py
+++ template/base/miner.py
@@ -45,7 +45,7 @@ class BaseMinerNeuron(BaseNeuron):
             )
 
         # The axon handles request processing, allowing validators to send this miner requests.
-        self.axon = bt.axon(wallet=self.wallet, port=self.config.axon.port)
+        self.axon = bt.axon(wallet=self.wallet, config=self.config)
 
         # Attach determiners which functions are called when servicing a request.
         bt.logging.info(f"Attaching forward function to miner axon.")
