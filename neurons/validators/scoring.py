c7a5766f89142e0fe8745cb3262b74b99161598a
0xMarx <mrmarx.game@gmail.com>
2024-02-25 21:18:20 +0100

    remove count_id_per_hotkey

--- neurons/validators/scoring.py
+++ neurons/validators/scoring.py
@@ -5,21 +5,17 @@ class Scorer:
     def __init__(self, config: ValidatorConfig):
         self.config = config
 
-    def calculate_score(self, network,  process_time, indexed_start_block_height, indexed_end_block_height, blockchain_last_block_height, miner_distribution, multiple_ips, multiple_run_ids):
+    def calculate_score(self, network,  process_time, indexed_start_block_height, indexed_end_block_height, blockchain_last_block_height, miner_distribution, multiple_ips):
         log =  (f'🔄 Network: {network} | ' \
                 f'Process time: {process_time:4f} | ' \
                 f'Indexed start block height: {indexed_start_block_height} | ' \
                 f'Indexed end block height: {indexed_end_block_height} | ' \
                 f'Blockchain last block height: {blockchain_last_block_height} | ' \
                 f'Miner distribution: {miner_distribution} | ' \
-                f'Multiple IPs: {multiple_ips} | ' \
-                f'Multiple run id: {multiple_run_ids} | ')
+                f'Multiple IPs: {multiple_ips} | ')
         bt.logging.info(log)
 
         if multiple_ips:
-            bt.logging.info(f"🔄 Final score: 0")
-            return 0
-        if multiple_run_ids:
             bt.logging.info(f"🔄 Final score: 0")
             return 0
 
