9faad4978abaf9e4957f127388ab3e4698ad2fe7
ronnie-samaroo <ron.sam624@gmail.com>
2024-03-16 14:34:42 +0100

    fix: test for validate challenge

--- tests/miners/test_graph_search.py
+++ tests/miners/test_graph_search.py
@@ -3,20 +3,31 @@ from unittest.mock import patch, MagicMock
 import sys
 import os
 
+from neurons.nodes.factory import NodeFactory
+from insights.protocol import NETWORK_BITCOIN, Challenge
 from neurons.miners.miner import Miner
 from neurons.miners.miner import wait_for_blocks_sync
 from neurons.miners.bitcoin.funds_flow.graph_search import GraphSearch
 
 class TestGraphSearch(unittest.TestCase):
     def test_solve_challenge(self):
+        node = NodeFactory.create_node(NETWORK_BITCOIN)
         graph_search = GraphSearch(
             graph_db_url=os.environ.get('GRAPH_DB_URL'),
             graph_db_user=os.environ.get('GRAPH_DB_USER'),
             graph_db_password=os.environ.get('GRAPH_DB_PASSWORD'),
         )
-        output = graph_search.solve_challenge(in_total_amount=203216, out_total_amount=200000, tx_id_last_4_chars="d946")
+
+        challenge = Challenge(in_total_amount=203216, out_total_amount=200000, tx_id_last_4_chars="d946")
+        expected_output = "93fa1edce68762615740fd35581fc337d508ca33e682057bed7b395b5d66d946"
+
+        output = graph_search.solve_challenge(in_total_amount=challenge.in_total_amount, out_total_amount=challenge.out_total_amount, tx_id_last_4_chars=challenge.tx_id_last_4_chars)
+        self.assertEqual(output, expected_output)
+        
+        is_valid = node.validate_challenge_response_output(challenge, expected_output)
+        self.assertEqual(is_valid, True)
+
         graph_search.close()
-        self.assertEqual(output, "93fa1edce68762615740fd35581fc337d508ca33e682057bed7b395b5d66d946")
         
     def test_get_min_max_block_height_cache(self):
         graph_search = GraphSearch(
