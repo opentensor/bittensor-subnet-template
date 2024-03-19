bd0f85c054867469a36355f50813455774408e13
wendellswa06 <wthompsonswa06@gmail.com>
2024-03-19 03:02:47 +0000

    get top miner uid

--- neurons/validators/utils/uids.py
+++ neurons/validators/utils/uids.py
@@ -28,6 +28,36 @@ def check_uid_availability(
     # Available otherwise.
     return True
 
+def get_best_miner_uid(
+    metagraph: "bt.metagraph.Metagraph", vpermit_tao_limit: int = 4096, exclude: List[int] = None
+) -> torch.LongTensor:
+    """Returns the best available miner UID from the metagraph.
+    Args:
+        metagraph (bt.metagraph.Metagraph): Metagraph object
+        vpermit_tao_limit (int): Validator permit tao limit
+        exclude (List[int]): List of uids to exclude from the random sampling.
+    Returns:
+        best_miner_uid (torch.LongTensor): The best miner UID.    
+    """
+    candidate_uids = []
+    for uid in range(metagraph.n.item()):
+        uid_is_available = check_uid_availability(
+            metagraph, uid, vpermit_tao_limit
+        )
+        uid_is_not_excluded = exclude is None or uid not in exclude
+
+        if uid_is_available:
+            if uid_is_not_excluded:
+                candidate_uids.append(uid)
+    # Consider both of incentive and trust score
+    values = [(uid, metagraph.I[uid] * metagraph.trust[uid]) for uid in candidate_uids]
+    
+    # Consider only incentive
+    # values = [(uid, metagraph.I[uid]) for uid in candidate_uids] 
+    best_miner_uid = torch.tensor(max(values, key=lambda x: x[1])[0])    
+    return best_miner_uid    
+    
+    
 
 def get_random_uids(
     self, k: int, exclude: List[int] = None
@@ -55,3 +85,10 @@ def get_random_uids(
     k = max(1, min(len(candidate_uids), k))
     uids = torch.tensor(random.sample(candidate_uids, k))
     return uids
+
+if __name__ == "__main__":
+    metagraph = bt.subtensor("local").metagraph(netuid=15)
+    bt.logging.info(f"best miner uid is {get_best_miner_uid(metagraph)}")
+    best_uid = get_best_miner_uid(metagraph)
+    for uid in best_uid:
+        bt.logging.info(uid)
