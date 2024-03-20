26ac4dacd7b231ca093c71d95523139076b9aa2d
ronnie-samaroo <ron.sam624@gmail.com>
2024-03-20 13:37:13 +0100

    test: add test case for llm build_query

--- tests/miners/test_llm.py
+++ tests/miners/test_llm.py
@@ -10,10 +10,19 @@ class TestLLM(unittest.TestCase):
         self.llm = OpenAILLM()
     
     def test_build_query(self):
-        query_text = "Return 20 transactions."
+        # test case 1
+        query_text = "Return 15 transactions outgoing from my address bc1q4s8yps9my6hun2tpd5ke5xmvgdnxcm2qspnp9r"
         query = self.llm.build_query_from_text(query_text=query_text)
-        print(f"query: {query}")
-
+        expected_query = {
+            "type": "search",
+            "target": "Transaction",
+            "where": {
+                "from_address": "bc1q4s8yps9my6hun2tpd5ke5xmvgdnxcm2qspnp9r"
+            },
+            "limit": 15,
+            "skip": 0
+        }
+        self.assertDictEqual(query, expected_query)
 
 if __name__ == '__main__':
     from dotenv import load_dotenv
