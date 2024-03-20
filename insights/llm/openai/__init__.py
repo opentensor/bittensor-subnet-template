9571a794987d456404ec28848460401723589cf7
ronnie-samaroo <ron.sam624@gmail.com>
2024-03-20 03:38:04 +0100

    fea: added OpenAILLM

---
+++ insights/llm/openai/__init__.py
@@ -0,0 +1,17 @@
+from insights.llm.base_llm import BaseLLM
+from insights.protocol import Query, QueryOutput
+
+
+class OpenAILLM(BaseLLM):
+    def __init__(self, api_key: str) -> None:
+        pass
+        
+    def build_query_from_text(self, query_text: str) -> Query:
+        pass
+        
+    def generate_text_response_from_query_output(self, query_output: QueryOutput) -> str:
+        pass
+        
+    def generate_llm_query_from_query(self, query: Query) -> str:
+        pass
+    
