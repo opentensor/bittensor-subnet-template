315d5dbf28747e1e9241da9b43f9b97cb960c843
ronnie-samaroo <ron.sam624@gmail.com>
2024-03-20 06:09:15 +0100

    feat: init openai llm

--- insights/llm/openai/__init__.py
+++ insights/llm/openai/__init__.py
@@ -1,10 +1,21 @@
+import os
+
 from insights.llm.base_llm import BaseLLM
 from insights.protocol import Query, QueryOutput
 
+from langchain_openai import ChatOpenAI
+
+from neurons.setup_logger import setup_logger
+
+logger = setup_logger("OpenAI LLM")
 
 class OpenAILLM(BaseLLM):
-    def __init__(self, api_key: str) -> None:
-        pass
+    def __init__(self) -> None:
+        api_key = os.getenv("OPENAI_API_KEY") or ""
+        if not api_key:
+            raise Exception("OpenAI_API_KEY is not set.")
+
+        self.chat = ChatOpenAI(api_key=api_key, temperature=0)
         
     def build_query_from_text(self, query_text: str) -> Query:
         pass
