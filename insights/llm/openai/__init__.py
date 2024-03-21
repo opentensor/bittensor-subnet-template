5406d536d46cf73f883f50f2e50e2ff6ca3f076f
ronnie-samaroo <ron.sam624@gmail.com>
2024-03-21 13:13:46 +0100

    fix: return Query instance instead of dict in build_query_from_text

--- insights/llm/openai/__init__.py
+++ insights/llm/openai/__init__.py
@@ -5,7 +5,7 @@ import bittensor as bt
 
 from insights.llm.base_llm import BaseLLM
 from insights.llm.prompts import query_schema
-from insights.protocol import Query, QueryOutput
+from insights.protocol import Query, QueryOutput, NETWORK_BITCOIN
 
 from langchain_openai import ChatOpenAI
 from langchain_core.messages import HumanMessage, SystemMessage
@@ -34,13 +34,22 @@ class OpenAILLM(BaseLLM):
         try:
             ai_message = self.chat.invoke(messages)
             query = json.loads(ai_message.content)
-            return query
+            return Query(
+                network=NETWORK_BITCOIN,
+                type=query["type"] if "type" in query else None,
+                target=query["target"] if "target" in query else None,
+                where=query["where"] if "where" in query else None,
+                limit=query["limit"] if "limit" in query else None,
+                skip=query["skip"] if "skip" in query else None,
+            )
         except Exception as e:
             bt.logging.error(f"LlmQuery build error: {e}")
             return None
         
     def interpret_result(self, query_text: str, result: dict) -> str:
-        pass
+        bt.logging.info(f"query_text: {query_text}")
+        bt.logging.info(f"result: {result}")
+        return "Interpreted result"
         
     def generate_llm_query_from_query(self, query: Query) -> str:
         pass
