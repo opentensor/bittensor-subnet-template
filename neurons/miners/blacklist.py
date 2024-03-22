f7882e98ea7c1563938d7f0e0d5de25fef64fae9
wendellswa06 <wthompsonswa06@gmail.com>
2024-03-22 01:55:41 +0000

    bug fix in query_blacklist

--- neurons/miners/blacklist.py
+++ neurons/miners/blacklist.py
@@ -29,7 +29,7 @@ def query_blacklist(self, synapse: protocol.Query) -> typing.Tuple[bool, str]:
         """
         hotkey = synapse.dendrite.hotkey
         # Check if the dendrite hotkey is not voting the sn or not.
-        if hotkey in self.miner_config.inmemory_hotkeys:
+        if hotkey not in self.miner_config.inmemory_hotkeys:
             return True, "Not support query synapse for this hotkey"
         
         is_blacklist, message = base_blacklist(self, synapse=synapse)
