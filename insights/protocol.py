f5e431b8b5746dbeb85c5042d41a584be6e6769c
wendellswa06 <wthompsonswa06@gmail.com>
2024-03-22 02:27:24 +0000

    Add interpreted_result to QueryOutput for LlmQuery Synapse

--- insights/protocol.py
+++ insights/protocol.py
@@ -87,7 +87,8 @@ class BlockCheck(BaseSynapse):
     output: BlockCheckOutput = None
 
 class QueryOutput(BaseModel):
-    result: Optional[Dict] = None
+    result: Optional[List[Dict]] = None
+    interpreted_result: Optional[str] = None
     error: Optional[str] = None
 
 class Query(BaseSynapse):
