4f5663760c8fd7e0455fff13257ee2e261c57961
Dan Rahmel <drahmel7@gmail.com>
2024-03-27 09:23:10 -0700

    DJR:Add variable additional sentences

--- tests_ap/test_miner_lib.py
+++ tests_ap/test_miner_lib.py
@@ -293,17 +293,41 @@ class ValidatorLib:
 
 
 class MinerLib:
-    async def doMining(self, convo, minerUid, dryrun=False):
-        print("MINERCONVO", convo, minerUid)
-        exampleTags = ["realistic", "business-minded", "conciliatory", "responsive", "caring", "understanding", "apologetic", "affectionate", "optimistic", "family-oriented"]
-        waitSec = random.randint(0, 3)
+    async def doMining(self, convoWindow, minerUid, dryrun=True):
+        #print("MINERCONVO", convoWindow, minerUid)
         out = {"uid":minerUid, "tags":[], "profiles":[], "convoChecksum":11}
+
         #print("Mine result: %ds" % (waitSec))
         if dryrun:
-            await asyncio.sleep(waitSec)
-            out["tags"].append(random.choice(exampleTags))
+            llml = LlmApi()
+            exampleSentences = [
+                "Who's there?",
+                "Nay, answer me. Stand and unfold yourself.",
+                "Long live the King!",
+                "Barnardo?",
+                "He.",
+                "You come most carefully upon your hour.",
+                "Tis now struck twelve. Get thee to bed, Francisco.",
+                "For this relief much thanks. Tis bitter cold, And I am sick at heart.",
+                "Have you had quiet guard?",
+                "Not a mouse stirring.",
+                "Well, good night. If you do meet Horatio and Marcellus, The rivals of my watch, bid them make haste.",
+                "I think I hear them. Stand, ho! Who is there?",
+                "Friends to this ground.",
+                "And liegemen to the Dane.",
+            ]
+            exchanges = copy.deepcopy(convoWindow)
+            exchanges.append([random.choice(exampleSentences), random.choice(exampleSentences)])
+            matches_dict = await llml.conversation_to_tags({"exchanges":exchanges})
+            tags = list(matches_dict.keys())
+            out["tags"] = tags
+            waitSec = random.randint(0, 3)
+            #await asyncio.sleep(waitSec)
+            if False:
+                out["tags"].append(random.choice(exampleTags))
         else:
             # TODO: Make this actually tag content
+            exampleTags = ["realistic", "business-minded", "conciliatory", "responsive", "caring", "understanding", "apologetic", "affectionate", "optimistic", "family-oriented"]
             out["tags"].append(random.choice(exampleTags))
         return out
 
