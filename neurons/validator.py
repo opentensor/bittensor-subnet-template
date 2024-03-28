653b9929197a0da6bc602e0d1f1de0b9f2443359
Dan Rahmel <drahmel7@gmail.com>
2024-03-28 21:19:37 +0000

    DJR:Validator loads and sorta runs

--- neurons/validator.py
+++ neurons/validator.py
@@ -19,6 +19,8 @@
 
 
 import time
+import os
+import hashlib
 
 # Bittensor
 import bittensor as bt
@@ -29,6 +31,11 @@ from template.validator import forward
 
 # import base validator class which takes care of most of the boilerplate
 from template.base.validator import BaseValidatorNeuron
+
+import conversationgenome.utils
+
+from conversationgenome.ValidatorLib import ValidatorLib
+
 
 
 class Validator(BaseValidatorNeuron):
@@ -46,7 +53,9 @@ class Validator(BaseValidatorNeuron):
         bt.logging.info("load_state()")
         self.load_state()
 
-        # TODO(developer): Anything specific to your use case you can do here
+        self.image_dir = './data/conversations/'
+        if not os.path.exists(self.image_dir):
+            os.makedirs(self.image_dir)
 
     async def forward(self):
         """
@@ -58,12 +67,45 @@ class Validator(BaseValidatorNeuron):
         - Updating the scores
         """
         # TODO(developer): Rewrite this function based on your protocol definition.
-        return await forward(self)
+        #return await forward(self)
+        # get_random_uids is an example method, but you can replace it with your own.
+        miner_uids = conversationgenome.utils.uids.get_random_uids(self, k=min(self.config.neuron.sample_size, self.metagraph.n.item()))
+        print("miner_uids", miner_uids)
 
+        # make a hash from the timestamp
+        filename = hashlib.md5(str(time.time()).encode()).hexdigest()
+
+        # Create a random image and load it.
+        #image_data = ocr_subnet.validator.generate.invoice(path=os.path.join(self.image_dir, f"{filename}.pdf"), corrupt=True)
+
+        # Create synapse object to send to the miner and attach the image.
+        #synapse = ocr_subnet.protocol.OCRSynapse(base64_image = image_data['base64_image'])
+
+        responses = None
+        rewards = None
+        # The dendrite client queries the network.
+        #responses = self.dendrite.query(
+        #    # Send the query to selected miner axons in the network.
+        #    axons=[self.metagraph.axons[uid] for uid in miner_uids],
+        #    # Pass the synapse to the miner.
+        #    synapse=synapse,
+        #    # Do not deserialize the response so that we have access to the raw response.
+        #    deserialize=False,
+        #)
+
+        # Log the results for monitoring purposes.
+        bt.logging.info(f"CGP Received responses: {responses}")
+
+        #rewards = ocr_subnet.validator.reward.get_rewards(self, labels=image_data['labels'], responses=responses)
+
+        bt.logging.info(f"CGP Scored responses: {rewards}")
+
+        # Update the scores based on the rewards. You may want to define your own update_scores function for custom behavior.
+        #self.update_scores(rewards, miner_uids)
 
 # The main function parses the configuration and runs the validator.
 if __name__ == "__main__":
     with Validator() as validator:
         while True:
-            bt.logging.info("Validator running...", time.time())
+            bt.logging.info("CGP running...", time.time())
             time.sleep(5)
