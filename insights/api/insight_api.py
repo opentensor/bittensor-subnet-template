d53c02513afd8c78f1e45bd187ddcba2880adbab
wendellswa06 <wthompsonswa06@gmail.com>
2024-03-29 14:18:38 +0000

    fix: remove metagraph resync part

--- insights/api/insight_api.py
+++ insights/api/insight_api.py
@@ -24,17 +24,16 @@ class APIServer:
         self.app = FastAPI()
         self.config = config
         self.wallet = bt.wallet(config=self.config)
-        self.subtensor = bt.subtensor(config=self.config)
         self.text_query_api = TextQueryAPI(wallet=self.wallet)
+        self.metagraph = bt.subtensor(config=self.config).metagraph(netuid=self.config.netuid)
         self.excluded_uids = []
         
         @self.app.get("/api/text_query")
         async def get_response(network:str, text: str):            
-            # select top miner
-            metagraph = self.subtensor.metagraph(self.config.netuid) # sync every request
-            top_miner_uids = get_top_miner_uids(metagraph, self.config.top_rate, self.excluded_uids)
+            # select top miner            
+            top_miner_uids = get_top_miner_uids(self.metagraph, self.config.top_rate, self.excluded_uids)
             bt.logging.info(f"Top miner UIDs are {top_miner_uids}")
-            top_miner_axons = await get_query_api_axons(wallet=self.wallet, metagraph=metagraph, uids=top_miner_uids)
+            top_miner_axons = await get_query_api_axons(wallet=self.wallet, metagraph=self.metagraph, uids=top_miner_uids)
             bt.logging.info(f"top miner axons: {top_miner_axons}")
             
             # get miner response
@@ -46,12 +45,12 @@ class APIServer:
                 timeout=self.config.timeout
                 )
             blacklist_axons = np.array(top_miner_axons)[blacklist_axon_ids]
-            blacklist_uids = np.where(np.isin(np.array(metagraph.axons), blacklist_axons))[0]
+            blacklist_uids = np.where(np.isin(np.array(self.metagraph.axons), blacklist_axons))[0]
             self.excluded_uids = np.union1d(np.array(self.excluded_uids), blacklist_uids)
             self.excluded_uids = self.excluded_uids.astype(int).tolist()
             
             # If the number of excluded_uids is bigger than top x percentage of the whole axons, format it.
-            if len(self.excluded_uids) > int(metagraph.n * self.config.top_rate):
+            if len(self.excluded_uids) > int(self.metagraph.n * self.config.top_rate):
                 bt.logging.info(f"Excluded UID list is too long")
                 self.excluded_uids = []
             bt.logging.info(f"excluded_uids are {self.excluded_uids}")
@@ -75,7 +74,7 @@ class APIServer:
                 )
             if not responses:
                 return "This hotkey is banned."
-            response = random.choice(responses)
+            response = random.choice(responses)        
             return response
                 
         @self.app.get("/")
