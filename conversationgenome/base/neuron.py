e72ffb5a0c8e8e37f0ae0e4952becc82d1b93aea
Dan Rahmel <drahmel7@gmail.com>
2024-03-29 17:23:13 +0000

    DJR:Removed spammy logs

--- conversationgenome/base/neuron.py
+++ conversationgenome/base/neuron.py
@@ -177,9 +177,7 @@ class BaseNeuron(ABC):
         )  # don't set weights if you're a miner
 
     def save_state(self):
-        bt.logging.warning(
-            "save_state() not implemented for this neuron. You can implement this function to save model checkpoints or other useful data."
-        )
+        pass
 
     def load_state(self):
         bt.logging.warning(
