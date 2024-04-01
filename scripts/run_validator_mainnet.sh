30636f6090b5664ded94490f8b20f8de64fedeb6
kamil <kamil.wojciechowski@chain-insights.ai>
2024-04-01 10:45:26 +0200

    validator startup scripts fixes

--- scripts/run_validator_mainnet.sh
+++ scripts/run_validator_mainnet.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 cd "$(dirname "$0")/../"
 export PYTHONPATH=$PWD
-python3 neurons/validators/validator.py --wallet.name "$WALLET_NAME" --wallet.hotkey "$WALLET_HOTKEY" --netuid "$NETUID" --subtensor.network "$SUBTENSOR_NETWORK" --subtensor.chain_endpoint "$SUBTENSOR_URL" --logging.trace
+python3 neurons/validators/validator.py --wallet.name "$WALLET_NAME" --wallet.hotkey "$WALLET_HOTKEY" --netuid "$NETUID" --subtensor.network "$SUBTENSOR_NETWORK" --subtensor.chain_endpoint "$SUBTENSOR_URL" --enable_api "$ENABLE_API" --logging.trace
