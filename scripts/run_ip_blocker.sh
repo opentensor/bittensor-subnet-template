7ae8d592e6e9bfd91bcc7460a6480a68a200cb99
kamil <kamil.wojciechowski@chain-insights.ai>
2024-04-26 12:25:59 +0200

    removed unused services

--- scripts/run_ip_blocker.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-cd "$(dirname "$0")/../"
-export PYTHONPATH=$PWD
-python3 neurons/miners/ip_blocker.py --wallet.name "$WALLET_NAME" --wallet.hotkey "$WALLET_HOTKEY" --netuid 15 --logging.debug --logging.trace
