e09a5ed70cb5810effe62efcd3aa68b7f35b8620
Ben <44178664+cxmplex@users.noreply.github.com>
2024-05-22 04:52:37 -0400

    this can be ignored if blacklist is fixed by my other pr

--- neurons/miner.py
+++ neurons/miner.py
@@ -59,7 +59,7 @@ class Miner(BaseMinerNeuron):
         the miner's intended operation. This method demonstrates a basic transformation of input data.
         """
         # TODO(developer): Replace with actual implementation logic.
-        # synapse.dummy_output = synapse.dummy_input * 2
+        synapse.dummy_output = synapse.dummy_input * 2
         return synapse
 
     async def blacklist(
