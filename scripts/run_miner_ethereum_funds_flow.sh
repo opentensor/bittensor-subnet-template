d59d2a5db09d8c611eabc770a3d94686b8790024
kamil <kamil.wojciechowski@chain-insights.ai>
2024-06-01 13:46:31 +0200

    renamed startup scripts

--- scripts/run_miner_ethereum_funds_flow.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-cd "$(dirname "$0")/../"
-export PYTHONPATH=$PWD
-python3 neurons/miners/miner.py --network ethereum --wallet.name "$WALLET_NAME" --wallet.hotkey "$WALLET_HOTKEY" --netuid 15 --subtensor.network "$SUBTENSOR_NETWORK" --miner_set_weights "$MINER_SET_WEIGHTS" --subtensor.chain_endpoint "$SUBTENSOR_URL" --logging.trace
