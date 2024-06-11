be0edc16c69e045ff553665e3abe4edc171f3cd1
opendansor <daniel@opentensor.dev>
2024-06-10 20:16:36 -0700

    Modify to be compatible with E2E testing

--- template/protocol.py
+++ template/protocol.py
@@ -54,7 +54,7 @@ class Dummy(bt.Synapse):
     # Required request input, filled by sending dendrite caller.
     dummy_input: int
 
-    # Optional request output, filled by recieving axon.
+    # Optional request output, filled by receiving axon.
     dummy_output: typing.Optional[int] = None
 
     def deserialize(self) -> int:
