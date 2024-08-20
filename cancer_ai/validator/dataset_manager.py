77db33eb2eca013c109e0a963faa1fa3ce804b6f
Wojtek Jurkowlaniec <wojtek.jurkowlaniec@gmail.com>
2024-08-20 18:00:30 +0200

    logging added

--- cancer_ai/validator/dataset_manager.py
+++ cancer_ai/validator/dataset_manager.py
@@ -1,20 +1,35 @@
 import os
 import shutil
 from pathlib import Path
-from .manager import SerializableManager
-from huggingface_hub import HfApi
-
 from typing import List, Tuple
-from async_unzip.unzipper import unzip
-from io import BytesIO
+
+from huggingface_hub import HfApi
+import bittensor as bt
+
+from .manager import SerializableManager
 from .utils import run_command
 from .dataset_handlers.image_csv import DatasetImagesCSV
 
+
+class DatasetManagerException(Exception):
+    pass
 
 class DatasetManager(SerializableManager):
     def __init__(
         self, config, competition_id: str, dataset_hf_id: str, file_hf_id: str
     ) -> None:
+        """
+        Initializes a new instance of the DatasetManager class.
+        
+        Args:
+            config: The configuration object.
+            competition_id (str): The ID of the competition.
+            dataset_hf_id (str): The Hugging Face ID of the dataset.
+            file_hf_id (str): The Hugging Face ID of the file.
+        
+        Returns:
+            None
+        """
         self.config = config
         self.competition_id = competition_id
         self.dataset_hf_id = dataset_hf_id
@@ -25,6 +40,7 @@ class DatasetManager(SerializableManager):
             self.config.models.dataset_dir, self.competition_id
         )
         self.data: Tuple[List, List] = ()
+        self.handler = None
 
     def get_state(self) -> dict:
         return {}
@@ -43,10 +59,12 @@ class DatasetManager(SerializableManager):
             repo_type="dataset",
         )
 
-    def delete_dataset(self):
+    def delete_dataset(self) -> None:
+        """Delete dataset from disk"""
         shutil.rmtree(self.local_compressed_path)
 
-    async def unzip_dataset(self):
+    async def unzip_dataset(self) -> None:
+        """Unzip dataset"""
         print("Unzipping dataset", self.local_compressed_path)
         os.system(f"rm -R {self.local_extracted_dir}")
         await run_command(
@@ -54,9 +72,10 @@ class DatasetManager(SerializableManager):
         )
         print("Dataset unzipped")
 
-    def set_dataset_handler(self):
+    def set_dataset_handler(self) -> None:
+        """Detect dataset type and set handler"""
         if not self.local_compressed_path:
-            raise Exception("Dataset not downloaded")
+            raise DatasetManagerException("Dataset not downloaded")
         # is csv in directory
         if os.path.exists(Path(self.local_extracted_dir, "labels.csv")):
             self.handler = DatasetImagesCSV(
@@ -66,13 +85,20 @@ class DatasetManager(SerializableManager):
             print("Files in dataset: ", os.listdir(self.local_extracted_dir))
             raise NotImplementedError("Dataset handler not implemented")
 
-    async def prepare_dataset(self):
+    async def prepare_dataset(self) -> None:
+        """Download dataset, unzip and set dataset handler"""
+
+        bt.logging.info("Downloading dataset")
         self.download_dataset()
+        bt.logging.info("Unzipping dataset")
         await self.unzip_dataset()
+        bt.logging.info("Setting dataset handler")
         self.set_dataset_handler()
+        bt.logging.info("Preprocessing dataset")
         self.data = await self.handler.get_training_data()
 
     async def get_data(self) -> Tuple[List, List]:
+        """Get data from dataset handler"""
         if not self.data:
-            raise Exception("Dataset not initalized ")
+            raise DatasetManagerException("Dataset not initalized ")
         return self.data
