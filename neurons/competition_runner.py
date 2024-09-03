9ba8bf694b8487117ca5a4f972ffbba4d5aeede0
Wojtek Jurkowlaniec <wojtek.jurkowlaniec@gmail.com>
2024-09-03 19:36:31 +0200

    competittion runner

--- neurons/competition_runner.py
+++ neurons/competition_runner.py
@@ -1,26 +1,71 @@
 from cancer_ai.validator.competition_manager import CompetitionManager
-from datetime import datetime
-
+from datetime import datetime, time, timedelta
+from pydantic import BaseModel
 import asyncio
 import json
-from datetime import datetime, timezone
+from datetime import datetime, timezone, timedelta
 import bittensor as bt
 from typing import List, Tuple, Dict
-from rewarder import Rewarder, WinnersMapping, CompetitionLeader
+from cancer_ai.validator.rewarder import Rewarder, WinnersMapping, CompetitionLeader
 
 # from cancer_ai.utils.config import config
 
 # TODO MOVE SOMEWHERE
 main_competitions_cfg = json.load(open("neurons/competition_config.json", "r"))
 
+MINUTES_BACK = 15
+
+
+class CompetitionRun(BaseModel):
+    competition_id: str
+    start_time: datetime
+    end_time: datetime | None = None
+
+
+class CompetitionRunLog(BaseModel):
+    runs: list[CompetitionRun]
+
+    def add_run(self, new_run: CompetitionRun):
+        """Add a new run and rotate the list if it exceeds 20 entries."""
+        self.runs.append(new_run)
+        if len(self.runs) > 20:
+            self.runs = self.runs[-20:]
+
+    def finish_run(self, competition_id: str):
+        """Finish the run with the given competition_id"""
+        for run in self.runs:
+            if run.competition_id == competition_id:
+                run.end_time = datetime.now(timezone.utc)
+
+    def was_competition_already_executed(
+        self, last_minutes: int = 5, competition_id: str = None
+    ):
+        """Check if competition was executed in last minutes"""
+        now_time = datetime.now(timezone.utc)
+        for run in self.runs:
+            if competition_id and run.competition_id != competition_id:
+                continue
+            if run.end_time and (now_time - run.end_time).seconds < last_minutes * 60:
+                return True
+        return False
+
+
+class CompetitionSchedulerConfig(BaseModel):
+    config: dict[datetime.time, CompetitionManager]
+
+    class Config:
+        arbitrary_types_allowed = True
+
+
 def config_for_scheduler(
     bt_config, hotkeys: List[str], test_mode: bool = False
-) -> Dict[str, CompetitionManager]:
+) -> CompetitionSchedulerConfig:
     """Returns CompetitionManager instances arranged by competition time"""
-    time_arranged_competitions = {}
+    scheduler_config = {}
     for competition_cfg in main_competitions_cfg:
-        for competition_time in competition_cfg["evaluation_time"]:
-            time_arranged_competitions[competition_time] = CompetitionManager(
+        for competition_time in competition_cfg["evaluation_times"]:
+            parsed_time = datetime.strptime(competition_time, "%H:%M").time()
+            scheduler_config[parsed_time] = CompetitionManager(
                 bt_config,
                 hotkeys,
                 competition_cfg["competition_id"],
@@ -30,31 +75,66 @@ def config_for_scheduler(
                 competition_cfg["dataset_hf_repo_type"],
                 test_mode=test_mode,
             )
-    return time_arranged_competitions
+
+    return scheduler_config
 
 
 async def run_competitions_tick(
-    competition_times: Dict[str, CompetitionManager],
+    competition_times: CompetitionSchedulerConfig,
+    run_log: CompetitionRunLog,
 ) -> Tuple[str, str] | None:
     """Checks if time is right and launches competition, returns winning hotkey and Competition ID. Should be run each minute."""
-    now_time = datetime.now(timezone.utc)
-    now_time = f"{now_time.hour}:{now_time.minute}"
-    bt.logging.debug(now_time)
-    competition_to_run = competition_times.get(now_time)
-    if not competition_to_run:
-        bt.logging.info("No competitions to run")
-        return 
-    bt.logging.info(
-        f"Running {competition_to_run.competition_id} at {now_time}"
-    )
-    winning_evaluation_hotkey = await competition_to_run.evaluate()
-    return (
-        winning_evaluation_hotkey,
-        competition_to_run.competition_id,
+
+    # getting current time
+    now = datetime.now(timezone.utc)
+    now_time = time(now.hour, now.minute)
+    bt.logging.info(f"Checking competitions at {now_time}")
+
+    for i in range(0, MINUTES_BACK):
+        # getting current time minus X minutes
+        check_time = (
+            datetime.combine(datetime.today(), now_time) - timedelta(minutes=i)
+        ).time()
+
+        # bt.logging.debug(f"Checking competitions at {check_time}")
+        if competition_manager := competition_times.get(check_time):
+            bt.logging.debug(
+                f"Found competition {competition_manager.competition_id} at {check_time}"
+            )
+        else:
+            continue
+
+        if run_log.was_competition_already_executed(
+            competition_id=competition_manager.competition_id
+        ):
+            bt.logging.info(
+                f"Competition {competition_manager.competition_id} already executed, skipping"
+            )
+            continue
+
+        bt.logging.info(f"Running {competition_manager.competition_id} at {now_time}")
+        run_log.add_run(
+            CompetitionRun(
+                competition_id=competition_manager.competition_id,
+                start_time=datetime.now(timezone.utc),
+            )
+        )
+        winning_evaluation_hotkey = await competition_manager.evaluate()
+        run_log.finish_run(competition_manager.competition_id)
+        # TODO log last run to WANDB
+        return (
+            winning_evaluation_hotkey,
+            competition_manager.competition_id,
+        )
+
+    bt.logging.debug(
+        f"Did not find any competitions to run for past {MINUTES_BACK} minutes"
     )
 
 
-async def competition_loop(scheduler_config: Dict[str, CompetitionManager], rewarder_config: WinnersMapping):
+async def competition_loop_not_used(
+    scheduler_config: CompetitionSchedulerConfig, rewarder_config: WinnersMapping
+):
     """Example of scheduling coroutine"""
     while True:
         competition_result = await run_competitions_tick(scheduler_config)
@@ -62,7 +142,9 @@ async def competition_loop(scheduler_config: Dict[str, CompetitionManager], rewa
         if competition_result:
             winning_evaluation_hotkey, competition_id = competition_result
             rewarder = Rewarder(rewarder_config)
-            updated_rewarder_config = await rewarder.update_scores(winning_evaluation_hotkey, competition_id)
+            updated_rewarder_config = await rewarder.update_scores(
+                winning_evaluation_hotkey, competition_id
+            )
             # save state of self.rewarder_config
             # save state of self.score (map rewarder config to scores)
             print(".....................Updated rewarder config:")
@@ -79,5 +161,5 @@ if __name__ == "__main__":
     hotkeys = []
     bt_config = {}  # get from bt config
     scheduler_config = config_for_scheduler(bt_config, hotkeys)
-    rewarder_config = WinnersMapping({},{})
-    asyncio.run(competition_loop(scheduler_config, rewarder_config))
+    rewarder_config = WinnersMapping(competition_leader_map={}, hotkey_score_map={})
+    asyncio.run(competition_loop_not_used(scheduler_config, rewarder_config))
