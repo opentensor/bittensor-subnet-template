9dded7b5802878037d232fd94a64d4cccd423092
Roman <roman@opentensor.dev>
2024-09-06 15:32:50 -0700

    black

--- template/base/neuron.py
+++ template/base/neuron.py
@@ -103,10 +103,12 @@ class BaseNeuron(ABC):
         self.step = 0
 
     @abstractmethod
-    async def forward(self, synapse: bt.Synapse) -> bt.Synapse: ...
+    async def forward(self, synapse: bt.Synapse) -> bt.Synapse:
+        ...
 
     @abstractmethod
-    def run(self): ...
+    def run(self):
+        ...
 
     def sync(self):
         """
